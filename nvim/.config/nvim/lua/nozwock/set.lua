494a4e21638ecbf5f7ad8f7d2251f16dfc9134ed
nozwock <57829219+nozwock@users.noreply.github.com>
2022-09-28 10:10:08 +0530

    change(nvim): add some sets

--- nvim/.config/nvim/lua/nozwock/set.lua
+++ nvim/.config/nvim/lua/nozwock/set.lua
@@ -1,3 +1,6 @@
+-- vim.opt.guicursor = ""
+
+-- line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -7,12 +10,23 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
 
 vim.opt.smartindent = true
 
+-- undohistory settings
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
 -- vim.opt.wrap = false
+
+-- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
+-- delays and poor user experience.
+vim.opt.updatetime = 50
 
 vim.g.mapleader = " "
 
