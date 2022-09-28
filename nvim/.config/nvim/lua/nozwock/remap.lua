272a2e1d72211e35c615368bdd53acf374c3198f
nozwock <57829219+nozwock@users.noreply.github.com>
2022-09-28 10:25:46 +0530

    change(nvim): add some basic important remaps

--- nvim/.config/nvim/lua/nozwock/remap.lua
+++ nvim/.config/nvim/lua/nozwock/remap.lua
@@ -1,4 +1,27 @@
-local nnoremap = require('nozwock.keymap').nnoremap
+local Remap = require("nozwock.keymap")
+local nnoremap = Remap.nnoremap
+local vnoremap = Remap.vnoremap
+local inoremap = Remap.inoremap
+local xnoremap = Remap.xnoremap
+local nmap = Remap.nmap
 
-nnoremap("<leader>pv", "<cmd>Ex<CR>") -- <CR> carriage return
+nnoremap("<leader>ex", "<cmd>Ex<CR>") -- <CR> carriage return
+nnoremap("<leader>u", ":UndotreeShow<CR>")
+
+-- greatest remap ever
+xnoremap("<leader>P", "\"_dP")
+nnoremap("<leader>p", "\"+p")
+vnoremap("<leader>p", "\"+p")
+
+-- next greatest remap ever : asbjornHaland
+nnoremap("<leader>y", "\"+y")
+vnoremap("<leader>y", "\"+y")
+nmap("<leader>Y", "\"+Y")
+
+nnoremap("<leader>d", "\"_d")
+vnoremap("<leader>d", "\"_d")
+
+vnoremap("<leader>d", "\"_d")
+
+inoremap("<C-c>", "<Esc>")
 
